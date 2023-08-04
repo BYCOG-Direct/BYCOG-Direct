f5639df1786b935135efeb8dee31d9d92a893a15
BYCOG-Direct <bycogdirect@gmail.com>
2023-08-04 16:54:11 +1000

    nav bar even cooler

--- components/nav.js
+++ components/nav.js
@@ -13,12 +13,12 @@ const Nav = () => {
      }
     window.addEventListener('scroll', NavColor)
     return (
-        <div className={color ? "duration-300 fixed flex justify-center gap-2 items-center py-3 px-3 right-20 top-10 z-20 rounded-full" : "duration-300 bg-gray shadow-xl fixed flex justify-center gap-2 items-center py-3 px-3 right-20 top-10 z-20 rounded-full"}>
-            <div className="flex justify-center gap-2 divide-x divide-white text-white text-center font-light text-xl">
+        <div className={color ? "duration-500 fixed flex justify-center gap-2 items-center py-3 px-3 right-20 top-10 z-20 rounded-full" : "duration-150 bg-gray shadow-xl fixed flex justify-center gap-2 items-center py-3 px-3 right-20 top-10 z-20 rounded-full"}>
+            <div className="flex justify-center divide-x-2 divide-tan text-white text-center font-light text-xl">
                 <Link href="/" className="py-1 px-3 hover:text-tan">Home</Link>
                 <Link href="/database" className="py-1 px-3 hover:text-tan">Database</Link>
                 <Link href="/essentials" className="py-1 px-3 hover:text-tan">Courses</Link>
-                <Link href="/visualiser" className="py-1 px-3 hover:text-tan">Tools</Link>
+                <Link href="/visualiser" className="py-1 pl-3 pr-1.5 hover:text-tan">Tools</Link>
             </div>
             <Link href="/">
                 <div className="flex justify-center items-center">
