9aa920ae2c5eb9b455d26734c28b173ae41a7071
BYCOG-Direct <bycogdirect@gmail.com>
2023-08-04 16:23:02 +1000

    cool nav

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,19 @@
+"use client"; // This is a client component
 import Link from "next/link"
 import Image from "next/image";
+import {useState} from 'react'
+
+
 
 const Nav = () => {
+    const [color, setColor] = useState(true)
+    const NavColor = () => {
+        if (window.scrollY >= 90) {setColor(false)}
+        else {setColor(true)}
+     }
+    window.addEventListener('scroll', NavColor)
     return (
-        <div className="flex justify-center gap-2 items-center py-3 px-3 fixed right-20 top-10 z-20 bg-gray rounded-full shadow-xl">
+        <div className={color ? "duration-300 fixed flex justify-center gap-2 items-center py-3 px-3 right-20 top-10 z-20 rounded-full" : "duration-300 bg-gray shadow-xl fixed flex justify-center gap-2 items-center py-3 px-3 right-20 top-10 z-20 rounded-full"}>
             <div className="flex justify-center gap-2 divide-x divide-white text-white text-center font-light text-xl">
                 <Link href="/" className="py-1 px-3 hover:text-tan">Home</Link>
                 <Link href="/database" className="py-1 px-3 hover:text-tan">Database</Link>
