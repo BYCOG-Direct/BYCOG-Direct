910b9ee83739251c73b76f0b63423e79b9636061
BYCOG-Direct <bycogdirect@gmail.com>
2023-08-07 12:48:53 +1000

    opacity banner

--- components/banner.js
+++ components/banner.js
@@ -1,4 +1,47 @@
+"use client"
 import Image from 'next/image';
+import { useEffect, useState } from "react";
+
+const Banner = () => {
+  const [scrollOpacity, setScrollOpacity] = useState(1);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const maxScroll = 300; // Adjust this value as needed
+    const opacity = 1 - scrollPosition / maxScroll;
+
+    // Make sure opacity never goes below 0
+    setScrollOpacity(Math.max(0, opacity));
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="bg-darkGray flex justify-center align-center flex-col pt-40 w-full">
+        <Image
+          className='w-[750px] h-full mx-auto'
+          src="/images/BYCOG_Direct_Logo.png"
+          height={0}
+          width={0}
+          sizes="100vw"
+          style={{ opacity: scrollOpacity }} // Set the opacity using inline style
+        />
+        <div className='mt-32 -mb-1 bg-tan w-full bannerClip h-[150px] z-0'></div>
+      </div>
+    </>
+  );
+}
+
+export default Banner;
+
+/* import Image from 'next/image';
+import {useEffect} from "react";
 
 const Banner = () => (
     <>
@@ -9,4 +52,4 @@ const Banner = () => (
     </>
 )
 
-export default Banner;
+export default Banner;*/
