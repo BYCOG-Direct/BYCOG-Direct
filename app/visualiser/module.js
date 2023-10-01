1d05056f1f946d34ffaa4d246ec4518bb07fcfc2
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-10-01 11:02:27 -0600

    multiple bycodes visualiser

---
+++ app/visualiser/module.js
@@ -0,0 +1,233 @@
+"use client"
+import React, { useEffect } from 'react'
+import { drawDiagram } from './diagramChanges';
+import { emptyType, defaultStyles } from './diagramChanges';
+
+const Module = ({ index, type, styles, setType, setStyles, deleteModule, duplicateModule }) => {
+
+    const handleCheckboxChange = (event) => {
+        // Get ID of the checkbox
+        const checkboxName = event.target.id;
+        // Get ID of the counterpart checkbox
+        const counterpartBoxName = event.target.getAttribute('data-uncheck');
+
+        const newType = {
+            ...type,                                // Copy the old fields
+            [counterpartBoxName]: false,            // Falsify the counterpart field
+            [checkboxName]: event.target.checked,   // Trueify the current field
+        }
+
+        setType(index, newType);
+    };
+
+    useEffect(() => {
+        const newDiagramStyling = drawDiagram(type, styles);
+        setStyles(index, newDiagramStyling);
+    }, [type])
+
+    const changeType = (event) => {
+        const moduleName = event.target.id;
+        const newType = {
+            ...type,
+            [moduleName]: event.target.value
+        }
+        setType(index, newType);
+    }
+
+    // Static Styles
+    const letterHost = "flex justify-center items-center flex-row flex-wrap rotate-[-45deg] transition duration-300 ease-in-out";
+    const others = "absolute top-[-67.5px] left-[132.5px] w-[135px] h-[135px] rotate-[45deg]";
+    const self = "absolute top-[332.5px] left-[132.5px] w-[135px] h-[135px] rotate-[45deg]";
+    const news = "absolute top-[132.5px] left-[332.5px] w-[135px] h-[135px] rotate-[45deg]";
+    const known = "absolute top-[132.5px] left-[-67.5px] w-[135px] h-[135px] rotate-[45deg]";
+
+    const middleCover = "absolute top-[37.5px] left-[37.5px] w-[325px] h-[325px] z-[-1] bg-[#FFF]";
+
+    const confer = "absolute top-[30px] right-[30px] w-[140px] h-[140px] z-[-2] transition duration-300 ease-in-out";
+    const search = "absolute bottom-[30px] right-[30px] w-[140px] h-[140px] z-[-2] transition duration-300 ease-in-out";
+    const revise = "absolute bottom-[30px] left-[30px] w-[140px] h-[140px] z-[-2] transition duration-300 ease-in-out";
+    const direct = "absolute top-[30px] left-[30px] w-[140px] h-[140px] z-[-2] transition duration-300 ease-in-out";
+
+    const left = "text-[41.06px] font-bold text-black";
+    const main = "text-[82.12px] font-bold text-black";
+    const right = "text-[41.06px] font-bold text-black";
+
+    const tetherStyle = "absolute w-[1000px] h-[1000px] max-w-none left-[-300px] top-[-300px]  transition duration-300 ease-in-out";
+    const checkboxStyle = "transition appearance-none w-5 h-5 border-2 border-black outline-none rounded-sm checked:bg-black checked:text-black align-middle";
+    const purpleCheckboxStyle = "transition appearance-none w-5 h-5 border-2 border-randPurp outline-none rounded-sm checked:bg-randPurp checked:text-randPurp align-middle";
+
+    // Clear type / Reset to empty type
+    const clear = () => {
+        setType(index, emptyType);
+        setStyles(index, defaultStyles);
+    };
+
+    const remove = () => {
+        deleteModule(index);
+    }
+
+    const duplicate = () => {
+        duplicateModule(index);
+    }
+
+    // Copy bycode to clipboard
+    const copyText = () => {
+        navigator.clipboard.writeText(`${textType1}:${textType2}`);
+    };
+
+  return (
+    <div className="container bg-white w-[500px] h-full rounded-[50px] relative flex justify-evenly items-center flex-col flex-wrap" id="container1">
+        {/* Output is the display (everything on the actual image) */}
+        <div className="output flex justify-center items-center w-[400px] h-[400px] scale-[0.4] m-0 mt-10">
+                
+                {/* The bycode background image */}
+            <img src="/images/visualiser/bg.png" className="background absolute z-[-3] w-[1000px] h-[1000px] max-w-none left-[-300px] top-[-300px] rounded-[10%]"/>
+            
+            {/* The cross tethers */}
+            <img src="/images/visualiser/main.png" className={`${tetherStyle} ${styles.crossTetherStyle}`} id="crossTethers"/>
+            <img src="/images/visualiser/sub.png" className={`${tetherStyle} ${styles.directTetherStyle}`}/>
+            <img src="/images/visualiser/sub.png" className={`${tetherStyle} ${styles.conferTetherStyle}`}/>
+            <img src="/images/visualiser/sub.png" className={`${tetherStyle} ${styles.searchTetherStyle}`}/>
+            <img src="/images/visualiser/sub.png" className={`${tetherStyle} ${styles.reviseTetherStyle}`}/>
+
+            {/* The arrow that exenuates your lead function */}
+            <div className="othersArrow" id="othersArrow"></div>
+            <div className="newsArrow" id="newsArrow"></div>
+            <div className="selfArrow" id="selfArrow"></div>
+            <div className="knownArrow" id="knownArrow"></div>
+            
+            {/* The four functions. Each is rotated 45 degrees, and then the internals are rotated back 45 degrees so that the square is rotated but the text isn't. */}
+            <div className={`${styles.othersBorder} ${others} ${styles.othersColour} ${letterHost}`} id="others">
+                <div className={letterHost}>
+                    <p className={left} id="othersLeftLetter">{styles.othersLeftLetter}</p>
+                    <p className={main} id="othersMainLetter">{styles.othersMainLetter}</p>
+                    <p className={right} id="othersRightLetter">E</p>
+                </div>
+            </div>
+
+            <div className={`${styles.newsBorder} ${news} ${styles.newsColour} ${letterHost}`} id="news">
+                <div className={letterHost}>
+                    <p className={left} id="newsLeftLetter">{styles.newsLeftLetter}</p>
+                    <p className={main} id="newsMainLetter">{styles.newMainLetter}</p>
+                    <p className={right} id="newsRightLetter">E</p>
+                </div>
+            </div>
+
+            <div className={`${styles.selfBorder} ${self} ${styles.selfColour} ${letterHost}`} id="self">
+                <div className={letterHost}>
+                    <p className={left} id="selfLeftLetter">{styles.selfLeftLetter}</p>
+                    <p className={main} id="selfMainLetter">{styles.selfMainLetter}</p>
+                    <p className={right} id="selfRightLetter">I</p>
+                </div>
+            </div>
+
+            <div className={`${styles.knownBorder} ${known} ${styles.knownColour} ${letterHost}`} id="known">
+                <div className={letterHost}>
+                    <p className={left} id="knownLeftLetter">{styles.knownLeftLetter}</p>
+                    <p className={main} id="knownMainLetter">{styles.knownMainLetter}</p>
+                    <p className={right} id="knownRightLetter">I</p>
+                </div>
+            </div>
+
+            {/* A white square that covers the unwanted sections of the reflex borders. */}
+            <div className={middleCover}></div>
+
+            {/* The reflex lines */}
+            <div className={`${styles.directBorder} ${direct}`} id="direct"></div>
+            <div className={`${styles.conferBorder} ${confer}`}  id="confer"></div>
+            <div className={`${styles.searchBorder} ${search}`}  id="search"></div>
+            <div className={`${styles.reviseBorder} ${revise}`}  id="revise"></div>
+        </div>
+
+        <div className='mt-4'>
+            <div className="flex justify-center items-center gap-5">
+                <img src="/images/visualiser/bin.png" id="binImg" className="w-8 h-8 cursor-pointer" title="Remove Bycode" onClick={remove}/>
+                <img src="/images/visualiser/clear.png" id="binImg" className="w-8 h-8 cursor-pointer" title="Clear Bycode" onClick={clear}/>
+                <div className="flex justify-center bg-tan rounded-xl py-1">
+                    <input className="w-[50px] h-[30px] border-none outline-none bg-transparent text-right" id="textType1" maxLength="2" value={type.textType1} onChange={changeType} />
+                    <input className="w-[10px] border-none outline-none bg-transparent text-center text-black" value=":" readOnly></input>
+                    <input className="w-[50px] h-[30px] border-none outline-none bg-transparent" id="textType2" maxLength="3" value={type.textType2} onChange={changeType} />
+                </div>
+                <img src="/images/visualiser/clipboard.png" id="clipboardImg" className="w-8 h-8 cursor-pointer" title="Copy to Clipboard" onClick={copyText}/>
+                <img src="/images/visualiser/personplus.png" id="clipboardImg" className="w-8 h-8 cursor-pointer" title="Duplicate Bycode" onClick={duplicate}/>
+            </div>
+            <div className="my-2 flex justify-center" id="ops">
+                <input className="p-1 rounded-full text-sm text-center" id="opsText" maxLength="16" onChange={changeType} value={type.opsType} placeholder="XX Xx/Xx XX/X(X)" />
+            </div>
+        </div>
+            
+        <div className="options flex justify-center items-center flew-row relative mb-5">
+            <div className="leftModule w-[150px] text-right">
+                <label htmlFor="identifierModule" className='text-[17px] mr-3'>Identifier</label>
+                <input className={checkboxStyle} type="checkbox" id="identifierModule" data-uncheck="evaluatorModule" checked={type.identifierModule} onChange={handleCheckboxChange}/>
+                <br/>
+                <br/>
+                <label htmlFor="selfModule" className='text-[17px] mr-3'>Self</label>
+                <input className={checkboxStyle} type="checkbox" id="selfModule" data-uncheck="othersModule" checked={type.selfModule} onChange={handleCheckboxChange}/>
+                <br/>
+                <label htmlFor="knownModule" className='text-[17px] mr-3'>Known</label>
+                <input className={checkboxStyle} type="checkbox" id="knownModule" data-uncheck="newsModule" checked={type.knownModule} onChange={handleCheckboxChange}/>
+                <br/>
+                <br/>
+                <label htmlFor="valueModule" className='text-[17px] mr-3'>Value</label>
+                <input className={checkboxStyle} type="checkbox" id="valueModule" data-uncheck="reasonModule" checked={type.valueModule} onChange={handleCheckboxChange}/>
+                <br/>
+                <label htmlFor="abstractModule" className='text-[17px] mr-3'>Abstract</label>
+                <input className={checkboxStyle} type="checkbox" id="abstractModule" data-uncheck="concreteModule" checked={type.abstractModule} onChange={handleCheckboxChange}/>
+                <br/>
+                <br/>
+                <label htmlFor="intermediateModule" className='text-[17px] mr-3'>Intermediate</label>
+                <input className={checkboxStyle} type="checkbox" id="intermediateModule" data-uncheck="extremeModule" checked={type.intermediateModule} onChange={handleCheckboxChange}/>
+                <br/>
+                <label htmlFor="developerModule" className='text-[17px] mr-3'>Developer</label>
+                <input className={checkboxStyle} type="checkbox" id="developerModule" data-uncheck="navigatorModule" checked={type.developerModule} onChange={handleCheckboxChange}/>
+            </div>
+            
+            <div className="rightModule w-[150px] text-left pl-2 ml-2 border-l-[2px]">
+                <input className={checkboxStyle} type="checkbox" id="evaluatorModule" data-uncheck="identifierModule" checked={type.evaluatorModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="evaluatorModule" className='text-[17px] ml-3'>Evaluator</label>
+                <br/>
+                <br/>
+                <input className={checkboxStyle} type="checkbox" id="othersModule" data-uncheck="selfModule" checked={type.othersModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="othersModule" className='text-[17px] ml-3'>Tribe</label>
+                <br/>
+                <input className={checkboxStyle} type="checkbox" id="newsModule" data-uncheck="knownModule" checked={type.newsModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="newsModule" className='text-[17px] ml-3'>New</label>
+                <br/>
+                <br/>
+                <input className={checkboxStyle} type="checkbox" id="reasonModule" data-uncheck="valueModule" checked={type.reasonModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="reasonModule" className='text-[17px] ml-3'>Reason</label>
+                <br/>
+                <input className={checkboxStyle}type="checkbox" id="concreteModule" data-uncheck="abstractModule" checked={type.concreteModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="concreteModule" className='text-[17px] ml-3'>Concrete</label>
+                <br/>
+                <br/>
+                <input className={checkboxStyle} type="checkbox" id="extremeModule" data-uncheck="intermediateModule" checked={type.extremeModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="extremeModule" className='text-[17px] ml-3'>Extreme</label>
+                <br/>
+                <input className={checkboxStyle} type="checkbox" id="navigatorModule" data-uncheck="developerModule" checked={type.navigatorModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="navigatorModule" className='text-[17px] ml-3'>Navigator</label>
+            </div>
+        </div>
+        <div className="text-randPurp"> Stance Expansion</div>
+        <div className="options flex justify-center items-center flew-row relative mb-5">
+            <div className="leftModule w-[150px] text-right">
+                <label htmlFor="solidSelfModule" className='text-[17px] mr-3 text-randPurp'>Solid Self</label>
+                <input className={purpleCheckboxStyle} type="checkbox" id="solidSelfModule" data-uncheck="solidOthersModule" checked={type.solidSelfModule} onChange={handleCheckboxChange}/>
+                <br/>
+                <label htmlFor="solidKnownModule" className='text-[17px] mr-3 text-randPurp'>Solid Known</label>
+                <input className={purpleCheckboxStyle} type="checkbox" id="solidKnownModule" data-uncheck="solidNewsModule" checked={type.solidKnownModule} onChange={handleCheckboxChange}/>
+            </div>
+            <div className="rightModule w-[150px] text-left pl-2 ml-2 border-l-[2px] border-randPurp">
+                <input className={purpleCheckboxStyle}type="checkbox" id="solidOthersModule" data-uncheck="solidSelfModule" checked={type.solidOthersModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="solidOthersModule" className='text-[17px] ml-3 text-randPurp'>Solid Tribe</label>
+                <br/>
+                <input className={purpleCheckboxStyle} type="checkbox" id="solidNewsModule" data-uncheck="solidKnownModule" checked={type.solidNewsModule} onChange={handleCheckboxChange}/>
+                <label htmlFor="solidNewsModule" className='text-[17px] ml-3 text-randPurp'>Solid New</label>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Module;
--- app/visualiser/page.js
+++ app/visualiser/page.js
@@ -1,27 +1,89 @@
 'use client';
-import Link from "next/link";
-import Script from 'next/script'
-import { hydrateRoot } from 'react-dom/client';
-import dynamic from 'next/dynamic'
-import React, {useEffect, useState} from 'react';
-import { Html, Head, Main, NextScript } from 'next/document'
-
-import Banner from '@components/banner';
-import Footer from '@components/footer';
-import Testimonials from '@components/testimonials';
-import Module from './components/module';
-
+import {useState} from 'react';
+import Module from './module';
+import { defaultStyles, emptyType } from "./diagramChanges";
+import Image from 'next/image';
 
 export default function Page() {
-    const [modules, setModules] = useState([]);
+    const [modules, setModules] = useState([{ styles: defaultStyles, type: emptyType }]);
+
+    const setType = (index, newType) => {
+        setModules((prevModules) => {
+            // Create a shallow copy of the original modules array
+            const updatedModules = [...prevModules];
+        
+            // Update the type for the specified index
+            updatedModules[index] = {
+              ...updatedModules[index],
+              type: newType,
+            };
+        
+            return updatedModules;
+        });
+    }
+
+    const setStyles = (index, newStyles) => {
+        setModules((prevModules) => {
+            // Create a shallow copy of the original modules array
+            const updatedModules = [...prevModules];
+        
+            // Update the type for the specified index
+            updatedModules[index] = {
+              ...updatedModules[index],
+              styles: newStyles,
+            };
+        
+            return updatedModules;
+        });  
+    }
+
+    const deleteModule = (index) => {
+        setModules((prevModules) => prevModules.filter((mod, i) => i != index));
+    }
+
+    const newModule = () => {
+        setModules((prevModules) => {
+            return [...prevModules, { type: emptyType, styles: defaultStyles }]
+        })
+    }
+
+    const duplicateModule = (index) => {
+        setModules((prevModules) => {
+            const updatedModules = [...prevModules];
+            const indexedModule = updatedModules[index];
+            return [...updatedModules, indexedModule];
+        })
+    }
 
     return (
-        <main>
-        
-            <div className="flex justify-center items-center flex-row flex-wrap" id="god">
-                <Module />
+        <main className={modules.length === 0 ? 'min-h-screen flex justify-center' : ''}>
+            <div className="flex justify-center items-center flex-col gap-7 flex-wrap mt-5" id="god">
+                <div>
+                    <div className='max-w-max p-3 mx-auto bg-gray rounded-full cursor-pointer' onClick={newModule}>
+                        <Image className='w-12 h-12' src="/images/visualiser/plus.svg" height={0} width={0} sizes="100vw" />
+                    </div>
+                    <div className='mt-1'>
+                        New Bycode
+                    </div>
+                </div>
+                <div className='flex justify-center items-center gap-10 flex-wrap'>
+                    {modules.map((mod, i) => (
+                        <Module 
+                            key={i}
+                            index={i} 
+                            setType={setType} 
+                            setStyles={setStyles} 
+                            type={mod.type}
+                            styles={mod.styles}
+                            deleteModule={deleteModule}
+                            duplicateModule={duplicateModule}
+                        />
+                    ))}
+                </div>
+                <div className="text-sm mb-10 w-3/4 mx-auto text-center">
+                    All web design, coding and functionality is Copyright © Divydation & Sectonic 2023. All rights reserved. The design and imagery of the Tilewheel is Copyright © Loglyn 2023. All rights reserved. 
+                </div>
             </div>
-
         </main>
     )
 }
