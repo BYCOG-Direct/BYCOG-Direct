d1b38b77aa9f53a0870a9d3057b6d202c21433bb
BYCOG-Direct <bycogdirect@gmail.com>
2023-11-06 02:06:51 +1100

    y

--- components/nav.js
+++ components/nav.js
@@ -64,34 +64,34 @@ const Nav = () => {
           // Top of screen nav bar closed
           ? (displayLongNav 
             ? "lg:hidden transition-all duration-500 bg-darkGray fixed flex justify-center gap-2 items-center p-3 mx-5 my-6 right-0 z-50 rounded-full"
-            : "lg:hidden transition-all duration-500 bg-gray shadow-xl fixed flex justify-center gap-2 items-center p-8 m-0 right-0 z-50 rounded-full rounded-t-none")
+            : "lg:hidden transition-all duration-300 bg-gray shadow-xl fixed flex justify-center gap-2 items-center p-5 m-3 right-0 z-50 rounded-full")
           // Top of screen nav bar Open
           : (displayLongNav 
             ? "lg:hidden transition-all duration-150 bg-gray shadow-xl fixed flex justify-center gap-2 items-center p-3 m-5 right-0 z-50 rounded-full"
-            : "lg:hidden transition-all duration-500 bg-gray shadow-xl fixed flex justify-center gap-2 items-center p-8 m-0 right-0 z-50 rounded-full rounded-t-none") 
+            : "lg:hidden transition-all duration-300 bg-gray shadow-xl fixed flex justify-center gap-2 items-center p-5 m-3 right-0 z-50 rounded-full") 
         }>
           <div className={
             displayLongNav
               ? "flex flex-col items-center h-auto w-12"
-              : "flex flex-col items-center text-white font-light h-auto w-12 -right-5 top-0"
+              : "flex flex-col items-center text-white font-light h-auto w-12 top-0"
           }
           // Conditionally include onClick only when displayLongNav is true
           { ...(displayLongNav ? { onClick: handleNavClick } : {}) }
           >
-            <Link href="/" className={displayLongNav ? " text-zero transition-all duration-500" : "hover:text-tan transition-all duration-500"}>Home</Link>
-            <div class={displayLongNav ? "w-[36px] h-[2px] bg-tan mx-[6px] my-[8px] transition-all duration-500" : "w-[36px] h-[2px] bg-tan mx-[6px] my-[10px] transition-all duration-500"}></div>
-            <Link href="/index" className={displayLongNav ? "text-zero transition-all duration-500" : "hover:text-tan transition-all duration-500"}>Index</Link>
-            <div class={displayLongNav ? "w-[36px] h-[2px] bg-tan mx-[6px] my-[5px] transition-all duration-500" : "w-[36px] h-[2px] bg-tan mx-[6px] my-[10px] transition-all duration-500"}></div>
-            <Link href="/learn/essentials" className={displayLongNav ? "text-zero transition-all duration-500" : "hover:text-tan transition-all duration-500"}>Learn</Link>
-            <div class={displayLongNav ? "w-[36px] h-[2px] bg-tan mx-[6px] my-[8px] transition-all duration-500" : "w-[36px] h-[2px] bg-tan mx-[6px] my-[10px] transition-all duration-500"}></div>                
-            <Link href="/visualiser" className={displayLongNav ? "text-zero transition-all duration-500" : "mb-3 hover:text-tan transition-all duration-500"}>Tools</Link>
+            <Link href="/" className={displayLongNav ? " text-zero transition-all duration-300" : "mt-2 hover:text-tan transition-all duration-300"}>Home</Link>
+            <div class={displayLongNav ? "w-7 h-[2px] bg-tan mx-[6px] my-[11px] transition-all duration-300" : "w-7 h-[2px] bg-tan mx-[6px] my-[10px] transition-all duration-300"}></div>
+            <Link href="/index" className={displayLongNav ? "text-zero transition-all duration-300" : "hover:text-tan transition-all duration-300"}>Index</Link>
+            <div class={displayLongNav ? "w-7 h-[2px] bg-tan mx-[6px] my-[-1px] transition-all duration-300" : "w-7 h-[2px] bg-tan mx-[6px] my-[10px] transition-all duration-300"}></div>
+            <Link href="/learn/essentials" className={displayLongNav ? "text-zero transition-all duration-300" : "hover:text-tan transition-all duration-300"}>Learn</Link>
+            <div class={displayLongNav ? "w-7 h-[2px] bg-tan mx-[6px] my-[11px] transition-all duration-300" : "w-7 h-[2px] bg-tan mx-[6px] my-[10px] transition-all duration-300"}></div>                
+            <Link href="/visualiser" className={displayLongNav ? "text-zero transition-all duration-300" : "mb-2 hover:text-tan transition-all duration-300"}>Tools</Link>
             <Link href="/profile">
               <div className="flex justify-center items-center">
                 <Image
                   className={
                     displayLongNav
-                      ? 'w-0 h-0 rounded-full border-0 transition-all duration-500'
-                      : 'w-10 h-10 rounded-full border-2 border-tan transition-all duration-500'
+                      ? 'w-0 h-0 rounded-full border-0 transition-all duration-300'
+                      : 'w-10 h-10 m-1 rounded-full border-2 border-tan transition-all duration-300'
                   }
                   src="/images/default_pfp.jpg"
                   height={0}
