c60e8d8a21af9d403365961dc74d5b7ca577ef68
BYCOG-Direct <bycogdirect@gmail.com>
2023-11-10 14:55:52 +1100

    website moment

--- app/learn/essentials/page.js
+++ app/learn/essentials/page.js
@@ -5,13 +5,17 @@ export default function Page() {
     return (
         <main>
             <div className="flex flex-row justify-center content-center">
-                <div className="bg-mediumBlack h-32 w-screen">
-                    <div className="flex flex-row text-center"> 
-                        <img src="/images/BYCOG_Direct_Logo.png" className="w-auto h-12 m-9 transition"/>
-                        <p className="text-white mt-10 text-4xl">Essentials</p>
+                <div className="bg-mediumBlack flex w-screen">
+                    <div className="flex flex-col md:flex-row text-center"> 
+                    <Link href="/">
+                        <img src="/images/BYCOG_Direct_Logo.png" className="w-[205.02px] h-12 mt-9 ml-9 md:m-9 transition"/>
+                    </Link>
+                    <div className="flex flex-col place-content-center w-screen md:w-auto"> 
+                        <p className="text-white text-3xl my-3">Essentials</p>
+                    </div>
                     </div>
                 </div>
-            </div>
+                </div>
 
             {/* <!-- Attention Hook --> */}
             <div className="flex flex-row justify-center content-center">
